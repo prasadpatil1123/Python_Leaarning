fdbb73ab6c2dd2c0880210ae889dce55ad8cc239
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2026-01-08 13:19:02 +0530

    second

--- python_Part_2/youtube_manager_app.py
+++ python_Part_2/youtube_manager_app.py
@@ -1,6 +1,27 @@
 
-def main():
+def load_data():
     pass
+
+def list_all_videos(video):
+    pass
+
+def main():
+    videos = load_data()
+    while True:
+        print("\n Youtube Manager App | Choose an option:")
+        print("1. View all videos")
+        print("2. Add a new video")
+        print("3. Update video details")
+        print("4. Delete a video")
+        print("5. Exit")
+        choice = input("Enter your choice (1-5): ")
+        
+        match choice:
+            case "1":
+                list_all_videos(videos)
+            case "2":
+                pass
 
 if __name__ == "__main__":
     main()
+    
